b2d0a332674770297b1f36af45df04f65212d491
ramp4 <rampatv13@gmail.com>
2020-01-23 11:49:13 +0300

    add production

---
+++ my-app/build/precache-manifest.1f51b8e7398e39dddc59130fc2cbba93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60afb43f147e4f0cabc8f55fdd5bac5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "769bc9e55019c17ce8e8",
+    "url": "/static/css/main.9d0726f2.chunk.css"
+  },
+  {
+    "revision": "8b177f2230cb3b8bf2d1",
+    "url": "/static/js/2.6f680b9c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6f680b9c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "769bc9e55019c17ce8e8",
+    "url": "/static/js/main.5f5917f0.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
